d7fdd272c1c654e06283f06adc76dcd41a3f1a84
kevinspr12-lang <kevinspr12@gmail.com>
2026-02-24 17:34:52 -0800

    añadi el tutorial

---
+++ POO/Enemigo.py
@@ -0,0 +1,26 @@
+class Enemigo:
+    tipo_enemigo: str
+    puntos_energia: int=10
+    ataque=1
+
+
+def_init_(self, tipo_enemigo, puntos_energia = 10, ataqu=1):
+    self.__ tipo_enemigo = tipo_enemigo
+    self.puntos_energia = puntos_energia
+    self.ataque = ataque
+
+
+def get_tipo_enemigo(self):
+    return self.__ tipo_enemigo
+
+
+def habla(self)
+    print(f"yo soy {self.__tipo_enemigo}. preparando para pelear")
+
+
+def camina(self):
+    print(f"{self.__tipo_enemigo} se mueve cerca de ti")
+
+
+def atacar(self):
+    print(f"{self.__tipo_enemigo} ataca con un {self.ataque} de daño")
